b478f22d0b59efc1a4b0dd65455f1bb7a14f7cc6
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2024-04-05 00:08:08 +0100

    Add project proposal and report

--- project-page/.eleventy.js
+++ project-page/.eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./assets/images");
+  eleventyConfig.addPassthroughCopy("./docs");
   eleventyConfig.addPassthroughCopy({"style.out.css": "style.css"});
 };
